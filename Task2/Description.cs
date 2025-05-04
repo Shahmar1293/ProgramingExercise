3f796a1df1de37a8b35dec76b10693a24480525a
Shahmar1293 <aghayevshahmar1@gmail.com>
2025-05-04 23:44:22 +0400

    Task Description Added

---
+++ Task2/Description.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Description
+    {
+        /*
+         * 1. Obyekt Yönülülü Proqramlaşdırma (OOP)
+Tapşırıq 1: Car adlı bir sinif yaradın. Sinifde Brand, Model, Year property-ləri olsun. Drive() adlı metod yazın, konsola "Car is driving" yazsın.
+Tapşırıq 2: Animal adlı abstrakt sinif yaradın. Speak() adlı abstrakt metod olsun. Dog və Cat sinifləri Animal-dan törəsin və Speak() metodunu override etsin.
+         */
+    }
+}
