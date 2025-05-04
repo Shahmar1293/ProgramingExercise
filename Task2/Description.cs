00ce6e9b926278f718e583a75d9fb6a6da447caa
Shahmar1293 <aghayevshahmar1@gmail.com>
2025-05-04 23:46:28 +0400

    Remove

--- Task2/Description.cs
+++ Task2/Description.cs
@@ -11,7 +11,6 @@ namespace Task2
         /*
          * 1. Obyekt Yönülülü Proqramlaşdırma (OOP)
 Tapşırıq 1: Car adlı bir sinif yaradın. Sinifde Brand, Model, Year property-ləri olsun. Drive() adlı metod yazın, konsola "Car is driving" yazsın.
-Tapşırıq 2: Animal adlı abstrakt sinif yaradın. Speak() adlı abstrakt metod olsun. Dog və Cat sinifləri Animal-dan törəsin və Speak() metodunu override etsin.
          */
     }
 }
