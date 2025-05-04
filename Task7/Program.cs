44bcfe55667d500b7c51e1dacc896ad1330ac378
Shahmar1293 <aghayevshahmar1@gmail.com>
2025-05-04 23:55:01 +0400

    Task7 Added

---
+++ Task7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Birinci rəqəmi daxil edin: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("İkinci rəqəmi daxil edin: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                double netice = a / b;
+                Console.WriteLine("Nəticə: " + netice);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Xəta: Sıfıra bölmək olmaz!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Xəta: Düzgün rəqəm daxil edilməyib.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Naməlum xəta baş verdi: " + ex.Message);
+            }
+
+            Console.WriteLine("Proqram bitdi.");
+
+        }
+    }
+}
