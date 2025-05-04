368b6adb0e1e17b9f8679c82cfe970e7ffeaf5b5
Shahmar1293 <aghayevshahmar1@gmail.com>
2025-05-05 00:05:56 +0400

    Description Added

---
+++ Task7/Description.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Description
+    {
+        /*
+         Tapşırıq 7: İstifadəçidən 2 rəqəm alın. Bölmə əməliyyatı edərkən "divide by zero" exception-u tuta bilən kod yazın.
+        */
+    }
+}
