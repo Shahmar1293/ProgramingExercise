60c722800ddb23b6b79edebc4e278997d71f77ae
Shahmar1293 <aghayevshahmar1@gmail.com>
2025-05-04 23:39:35 +0400

    First Task Done

---
+++ Task2/CarRent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+
+
+{
+    public class CarRent
+    {
+        // Property-lər
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        // Metod
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task2
+
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+            CarRent myCar = new CarRent
+            {
+                Brand = "Toyota",
+                Model = "Corolla",
+                Year = 2020
+            };
+
+            myCar.Drive();  // Konsolda: Car is driving
         }
     }
 }
